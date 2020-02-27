1d62cf0db86a9c9b9eae6e9045b9b515a37df328
davidecasuccio <davide.casuccio@studenti.unicampania.it>
2020-02-27 17:45:59 +0100

    Cancellate cartelle inutili

--- production_split.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import pandas as pd
-
-df = pd.read_csv('pvpanels_production/kn10/kn10.csv', sep=" ")
-df['time']=pd.to_datetime(df["time"],unit='s')
-df.to_csv('pvpanels_production/kn10/production_kn10.csv',index=False)
-
