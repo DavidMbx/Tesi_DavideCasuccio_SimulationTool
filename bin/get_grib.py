81ea7f8f56f59cdb9d2cfe6d20574f508e3d0fd1
davidecasuccio <davide.casuccio@studenti.unicampania.it>
2020-02-27 12:37:26 +0100

    Creato il programma per creare un file csv ogni 6 ore: 0,6,12,18

---
+++ bin/get_grib.py
@@ -0,0 +1,36 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'reanalysis-era5-land',
+    {
+        'area': '47/9/47/9',  # North, West, South, East. Default: global
+         # Latitude/longitude grid: east-west (longitude) and north-south resolution (latitude). Default: 0.25 x 0.25
+
+        'variable': [
+            '2m_temperature', 'soil_temperature_level_1',
+            'surface_net_solar_radiation', 'surface_solar_radiation_downwards',
+        ],
+        'year': ['2015'],
+        'month': [
+            '10',
+        ],
+        'day': [
+            '11',
+
+
+        ],
+        'time': [
+            '00:00', '01:00', '02:00',
+            '03:00', '04:00', '05:00',
+            '06:00', '07:00', '08:00',
+            '09:00', '10:00', '11:00',
+            '12:00', '13:00', '14:00',
+            '15:00', '16:00', '17:00',
+            '18:00', '19:00', '20:00',
+            '21:00', '22:00', '23:00',
+        ],
+        'format': 'grib',
+    },
+    'download.grib')
